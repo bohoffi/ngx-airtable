a7582931070d2bb035c0319dd43a5119e0720bd2
Basti Hoffmann <bo.hoffi@gmail.com>
2019-09-12 16:07:06 +0200

    updated project files

--- projects/ngx-airtable/src/public_api.ts
+++ projects/ngx-airtable/src/public_api.ts
@@ -2,11 +2,11 @@
  * Public API Surface of ngx-airtable
  */
 export * from './lib/ngx-airtable.module';
-export {AirtableConfiguration, SelectParams, Link} from './lib/interfaces';
-export {SortDirection} from './lib/types';
-export {Airtable} from './lib/node-port/airtable';
-export {Base} from './lib/node-port/base';
-export {Table} from './lib/node-port/table';
-export {Query} from './lib/node-port/query';
+export * from './lib/interfaces';
+export * from './lib/types';
+export * from './lib/node-port/airtable';
+export * from './lib/node-port/base';
+export * from './lib/node-port/table';
+export * from './lib/node-port/query';
 export * from './lib/reactive/LinkedTable';
 export * from './lib/reactive/LinkedQuery';
