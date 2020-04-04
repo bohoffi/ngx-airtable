1e26eabb11676b5db4ffdc5312f3786ce579d9ea
Basti Hoffmann <bo.hoffi@gmail.com>
2020-04-04 14:29:56 +0200

    chore: update public api

--- projects/ngx-airtable/src/public_api.ts
+++ projects/ngx-airtable/src/public_api.ts
@@ -2,13 +2,31 @@
  * Public API Surface of ngx-airtable
  */
 export * from './lib/ngx-airtable.module';
-export * from './lib/interfaces';
-export * from './lib/types';
-export * from './lib/node-port/airtable';
-export * from './lib/node-port/base';
-export * from './lib/node-port/table';
-export * from './lib/node-port/query';
-export * from './lib/node-port/runaction';
-export * from './lib/node-port/operators';
-export * from './lib/reactive/LinkedTable';
-export * from './lib/reactive/LinkedQuery';
+// export * from './lib/interfaces';
+// export * from './lib/types';
+// export * from './lib/node-port/base';
+// export * from './lib/node-port/table';
+// export * from './lib/node-port/query';
+// export * from './lib/node-port/runaction';
+// export * from './lib/reactive/LinkedTable';
+// export * from './lib/reactive/LinkedQuery';
+
+// NEW API
+// interfaces
+export * from './lib/interfaces/airtable-configuration';
+export * from './lib/interfaces/params';
+export * from './lib/interfaces/select-params';
+export * from './lib/interfaces/sort-param';
+export * from './lib/interfaces/table-options';
+
+// operators
+export * from './lib/operators/base';
+export * from './lib/operators/execute';
+export * from './lib/operators/find';
+export * from './lib/operators/first-page';
+export * from './lib/operators/select';
+export * from './lib/operators/table';
+
+// services
+export * from './lib/services/airtable';
+
